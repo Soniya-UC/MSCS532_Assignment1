bcf8352fe8db9aa0ca10ddceaa298ab237d8aab6
Soniya-UC <spadamati34226@ucumberlands.edu>
2025-05-18 00:44:43 -0500

    added comments with final code

--- insertionSort.py
+++ insertionSort.py
@@ -1,16 +1,16 @@
 def iSort_desc(arr):
   
-    for i in range(1, len(arr)):
+    for i in range(1, len(arr)): # Start from the second element
         key = arr[i]
-        j = i - 1
+        j = i - 1 #comparing with the previous element
 
         while j >= 0 and arr[j] < key:
-            arr[j + 1] = arr[j]
+            arr[j + 1] = arr[j]  # Shifting the smaller element to the right
             j -= 1
         
-        
+         # Inserting the key at the correct position
         arr[j + 1] = key
-        
+
 arr = [7,1,3,8,5,2,4,6]
 print("initial array:", arr)
 iSort_desc(arr)
